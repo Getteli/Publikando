b3cc0c6db6dd98c357d3182f1a3cbd3dde44f87a
Getteli <douglas.araujo.01@outlook.com>
2023-05-21 11:44:00 -0300

    update readme, ajustes nas pastas

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# Publikando
-Site da publikando
+<p align="center"><a href="https://getteli.github.io/Publikando/" target="_blank"><img src="https://getteli.github.io/Publikando/assets/images/logo.png" width="150px"></a></p>
+
+<h2 align="center">
+    <b>Publikando - site principal</b>
+</h2>
+
+<p align="center">
+    <a href="#"><img src="https://img.shields.io/badge/HTML-5-%23f7df1e"></a>
+    <a href="#"><img src="https://img.shields.io/badge/CSS-3-%232760e5"></a>
+    <a href="#"><img src="https://img.shields.io/badge/language-JS-%23f7df1e" alt="Language"></a>
+    <a href="#"><img src="https://img.shields.io/badge/license-MIT-green" alt="License"></a>
+</p>
+
+<p>
+    Site para a startup de um grande amigo meu. Este é o site principal de sua empresa (focada em design, logos e sites).
+</p>
+
+[Veja o site aqui](https://getteli.github.io/Publikando/)
+
+## Tecnologias usadas:
+- **[Javascript](https://developer.mozilla.org/pt-BR/docs/Web/JavaScript)**;
+- **[Jquery](https://jquery.com/)**;
---
+++ assets/scripts/index.completo.js
@@ -0,0 +1,158 @@
+$(document).ready(function(){
+	// tooltip
+	$('.tooltipped').tooltip();
+
+	$('#contato').mask('(00) 0 0000-0000');
+
+	// modal
+	const ElemensModal = document.querySelectorAll(".modal");
+	const InstanceModal = M.Modal.init(ElemensModal);
+
+	// textarea form contato
+	$('#textarea').val('');
+	M.textareaAutoResize($('#textarea'));
+
+	// toast
+	const ElemensToast = document.querySelector("#toast");
+	const InstanceToast = (msg) =>{
+		M.toast({
+			html: msg,
+			classes: "rounded toastMobile"
+		})
+	}
+
+	// validade email
+	function isEmail(email) {
+		var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		return regex.test(email);
+	}
+
+	// inicia a mensagem assim que o site carregar
+	InstanceToast("Conheça nossos serviços em poucos cliques !");
+
+	// function enviar msg
+	$(".sendContato").on('click', function(e){
+
+		// nome
+		if($(this)[0].form.elements[0].value == null
+			|| $(this)[0].form.elements[0].value == ""){
+			$(this)[0].form.elements[0].focus();
+			$(".p_warning").eq(0).show();
+			return false;
+		}
+		// email
+		if($(this)[0].form.elements[1].value == null
+			|| $(this)[0].form.elements[1].value == ""
+			|| !isEmail($(this)[0].form.elements[1].value)){
+			$(this)[0].form.elements[1].focus();
+			$(".p_warning").eq(1).show();
+			return false;
+		}
+		// mensagem
+		if($(this)[0].form.elements[3].value == null
+			|| $(this)[0].form.elements[3].value == ""){
+			$(this)[0].form.elements[3].focus();
+			$(".p_warning").eq(2).show();
+			return false;
+		}
+
+		if(!$(".p_warning").eq(0).is(':empty')){
+			$(".p_warning").eq(0).hide();
+		}
+		
+		if(!$(".p_warning").eq(1).is(':empty')){
+			$(".p_warning").eq(1).hide();
+		}
+		
+		if(!$(".p_warning").eq(2).is(':empty')){
+			$(".p_warning").eq(2).hide();
+		}	
+
+		$(".sendContato").addClass("disabled");
+		$(".lbl_form").show();
+		$(".lbl_form_padrao").hide();
+
+		e.preventDefault(); // avoid to execute the actual submit of the form.
+		var form = $(".form_contact").serialize();
+		var url = "assets/scripts/send-mail.min.php";
+		$.ajax({
+			type: "POST",
+			url: url,
+			data: form, // serializes the form's elements.
+			success: function (data) {
+				InstanceToast("mensagem enviada com sucesso !");
+			},
+			error: function (data) {
+				InstanceToast("erro ao enviar mensagem, verifique e tente novamente.");
+			},
+			complete: function () {
+				$(".sendContato").removeClass("disabled");
+				$(".lbl_form").hide();
+				$(".lbl_form_padrao").show();
+			},
+		});
+	});
+
+	// function enviar msg mobile
+	$(".sendContatoMob").on('click', function(e){
+
+		// nome
+		if($(this)[0].form.elements[0].value == null
+			|| $(this)[0].form.elements[0].value == ""){
+			$(this)[0].form.elements[0].focus();
+			$(".p_warning").eq(0).show();
+			return false;
+		}
+		// email
+		if($(this)[0].form.elements[1].value == null
+			|| $(this)[0].form.elements[1].value == ""
+			|| !isEmail($(this)[0].form.elements[1].value)){
+			$(this)[0].form.elements[1].focus();
+			$(".p_warning").eq(1).show();
+			return false;
+		}
+		// mensagem
+		if($(this)[0].form.elements[3].value == null
+			|| $(this)[0].form.elements[3].value == ""){
+			$(this)[0].form.elements[3].focus();
+			$(".p_warning").eq(2).show();
+			return false;
+		}
+
+		if(!$(".p_warning").eq(0).is(':empty')){
+			$(".p_warning").eq(0).hide();
+		}
+		
+		if(!$(".p_warning").eq(1).is(':empty')){
+			$(".p_warning").eq(1).hide();
+		}
+		
+		if(!$(".p_warning").eq(2).is(':empty')){
+			$(".p_warning").eq(2).hide();
+		}	
+
+		$(".sendContatoMob").addClass("disabled");
+		$(".lbl_form").show();
+		$(".lbl_form_padrao").hide();
+
+		e.preventDefault(); // avoid to execute the actual submit of the form.
+		var form = $(".form_contactMob").serialize();
+		var url = "assets/scripts/send-mail.min.php";
+		$.ajax({
+			type: "POST",
+			url: url,
+			data: form, // serializes the form's elements.
+			success: function (data) {
+				InstanceToast("mensagem enviada com sucesso !");
+			},
+			error: function (data) {
+				InstanceToast("erro ao enviar mensagem, verifique e tente novamente.");
+			},
+			complete: function () {
+				$(".sendContatoMob").removeClass("disabled");
+				$(".lbl_form").hide();
+				$(".lbl_form_padrao").show();
+			},
+		});
+	});
+});
